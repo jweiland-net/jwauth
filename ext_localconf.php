c215186f8c4ebf36eb68c78b43e26c0d23ab511a
Stefan Froemken <froemken@gmail.com>
2019-04-10 18:36:52 +0200

    lower case TRUE/FALSE

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,12 +4,12 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Check login with each Request
-$GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_alwaysFetchUser'] = TRUE;
+$GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_alwaysFetchUser'] = true;
 // Following line allows us to fetch the user data from Session instead of Database.
 // But as long as we don't have a real login, we can't deactivate the service directly with help of
 // deactivating the extension. The session is still valid.
 // That's why I think it's better to do the check with each request
-//$GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] = TRUE;
+//$GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] = true;
 
 // add service to get a fe_user with defined IP-Address
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
@@ -20,7 +20,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_alwaysFetchUser'] = T
         'title' => 'FE IP authentication',
         'description' => 'Login to FE with help of IP',
         'subtype' => 'getUserFE,authUserFE',
-        'available' => TRUE,
+        'available' => true,
         'priority' => 80,
         // must be higher than \TYPO3\CMS\Sv\AuthenticationService (50) and rsaauth (60) but lower than OpenID (75)
         'quality' => 80,
